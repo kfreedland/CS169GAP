4b2fc8e12fa8e4153420d373bc5c2d5e7c4bef35
Evan Layman <evan_layman@berkeley.edu>
2013-02-26 16:23:22 -0800

    fixing production config

--- config/production.js
+++ config/production.js
@@ -18,8 +18,8 @@
 
 var config = {
   detailedErrors: false
-, hostname: null
-, port: 4000
+, hostname: '0.0.0.0'
+, port: process.env.PORT
 
 // Using Postgres as the default, with only a Postgres DB
 , model: {
