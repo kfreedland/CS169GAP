500892067c3b331a1c651287a18269f7792af9c5
Kurtis <kurtis@kurtis.(none)>
2013-03-04 15:03:18 -0800

    added the controller logic for activity search and made the readme format that we should use for changelogs for major parts

--- README.md
+++ README.md
@@ -1,20 +1,30 @@
 CS169GAP
 ========
+Info needed to Run App:
+	Latest version has facebook passport auth: 
+	sudo npm install passport-facebook
+	Heroku URL: thawing-hamlet-4089.herokuapp.com
 
-Heroku URL: thawing-hamlet-4089.herokuapp.com
+Changelog Entry 1:
+	//required fields that the client checks is valid
+    queryInfo.flag = params.flag;
+    queryInfo.begin_date = params.begin_date;
+    queryInfo.end_date = params.end_date;
+    queryInfo.latitude = params.latitude;
+    queryInfo.longitude = params.longitude;
 
-ActivityModel Fields:
-Name: string
-Description: string
-time1: time
-time2: time
-//startEnd, openClose, anyTime, dayTime, nightTime
-flag: string
-begin_date: date
-end_date: date
-low_price: int
-high_price: int
-low_num_participants: int
-high_num_participants: int
-latitude: number
-longitude: number
+Changelog Entry 0:
+	//ActivityModel Fields:
+	Name: string
+	Description: string
+	time1: time
+	time2: time
+	flag: string -vals=startEnd, openClose, anyTime, dayTime, nightTime
+	begin_date: date
+	end_date: date
+	low_price: int
+	high_price: int
+	low_num_participants: int
+	high_num_participants: int
+	latitude: number
+	longitude: number
--- app/controllers/activities.js
+++ app/controllers/activities.js
@@ -1,6 +1,87 @@
 var Activities = function () {
   this.respondsWith = ['html', 'json', 'xml', 'js', 'txt'];
+  /** data is of the following form
+  Name: string
+  Description: string
+  time1: time
+  time2: time
+  flag: string startEnd, openClose, anyTime, dayTime, nightTime
+  begin_date: date
+  end_date: date
+  low_price: int
+  high_price: int
+  low_num_participants: int
+  high_num_participants: int
+  latitude: number
+  longitude: number
+  **/
+  this.search = function (req, resp, params)
+  {
+    var self = this;
+    console.log("Activities controller");
+    //We want to assure ourselves that the model only gets the relevant params and not anything extra
+    //I use default values for fields that are left blank to make for easier queries in the model
+    queryInfo = {};
+    //defualts to no name
+    queryInfo.name="";
+    if (params.name)
+    {
+      queryInfo.name = params.name;
+    }
 
+    today = new Date();
+    currentTime = today.getUTC
+    //defaults to right now
+    queryInfo.time1 = today.toLocaleTimeString();
+    
+    tomorrow = new Date();
+    tomorrow.setDate(today.getDate()+1);
+    //defaults to 24 hours ahead of today;
+    queryInfo.time2 = tomorrow.toLocaleTimeString();
+
+    if(params.time1 && params.time2)
+    {
+      queryInfo.time1 = params.time1;
+      queryInfo.time2 = params.time2;
+    }
+
+    //required fields that the client checks is valid
+    queryInfo.flag = params.flag;
+    queryInfo.begin_date = params.begin_date;
+    queryInfo.end_date = params.end_date;
+    queryInfo.latitude = params.latitude;
+    queryInfo.longitude = params.longitude;
+
+    queryInfo.low_price = 0;
+    if(params.low_price)
+    {
+      queryInfo.low_price = params.low_price;
+    }
+
+    queryInfo.high_price = Number.MAX_VALUE;
+    if(params.high_price)
+    {
+      queryInfo.high_price = params.high_price;
+    }
+
+    queryInfo.low_num_participants = 0;
+    if(params.low_num_participants)
+    {
+      queryInfo.low_num_participants = params.low_num_participants;
+    }
+
+    queryInfo.high_num_participants = Number.MAX_VALUE;
+    if(params.high_num_participants)
+    {
+      queryInfo.high_num_participants = params.high_num_participants;
+    }
+
+    geddy.model.Activity.search(queryInfo, function(responseDict)
+    {
+      console.log("ERRCODE IS: "+responseDict);
+      self.respond(responsedict, {format: 'json'});
+    });
+  }
   this.index = function (req, resp, params) {
     var self = this;
 
