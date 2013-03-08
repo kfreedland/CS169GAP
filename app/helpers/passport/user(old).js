cae9e577d3aa0af9ac57820978cc729559988fea
Evan Layman <evan_layman@berkeley.edu>
2013-03-08 12:33:31 -0800

    saved old files

---
+++ app/helpers/passport/user(old).js
@@ -0,0 +1,95 @@
+var strategies = require('./strategies')
+  , User = geddy.model.User
+  , Passport = geddy.model.Passport
+  , user
+  , _findOrCreateUser;
+
+_findOrCreateUser = function (passport, profile, callback) {
+  passport.getUser(function (err, data) {
+    var user
+      , userData;
+
+    if (err) {
+      console.log("_findOrCreateUser got error: " + err);
+      callback(err, null);
+    }
+    else {
+      if (!data) {
+        userData = strategies[passport.authType].parseProfile(profile);
+        console.log("Got userData: ");
+        for (var key in userData){
+          console.log(key + " : " + userData[key]);
+        }
+        user = User.create(userData);
+        // User won't have all required fields, force-save
+        user.save({force: true}, function (err, data) {
+          if (err) {
+            console.log("_findOrCreateUser user.save1 got error: " + err);
+            callback(err, null);
+          }
+          else {
+            user.addPassport(passport);
+            user.save({force: true}, function (err, data) {
+            //console.dir(user);
+            //console.dir(passport);
+              if (err) {
+                console.log("_findOrCreateUser user.save2 after addPassport got error: " + err);
+                callback(err, null);
+              }
+              else {
+                callback(null, user);
+              }
+            });
+          }
+        });
+      }
+      else {
+        user = data;
+        callback(null, user);
+      }
+    }
+  });
+};
+
+user = new (function () {
+
+  this.lookupByPassport = function (authType, profile, callback) {
+    var typeData = strategies[authType]
+      , key = String(profile[typeData.keyField]) // Important, want to use strings, not nums
+      , passport;
+
+    passport = Passport.first({authType: authType, key: key}, function (err, data) {
+      var pass;
+      if (err) {
+        console.log("Passport.first got error: " + err);
+        callback(err, null);
+      }
+      else {
+        console.log("Passport.first didn't get error and got data: " + data);
+        if (!data) {
+          pass = Passport.create({
+            authType: authType
+          , key: key
+          });
+          pass.save(function (err, data) {
+            if (err) {
+              console.log("Passport.save errored: " + err);
+              callback(err, null);
+            }
+            else {
+              console.log("Passport.save didn't error. Going to _findOrCreateUser");
+              _findOrCreateUser(pass, profile, callback);
+            }
+          });
+        }
+        else {
+          pass = data;
+          _findOrCreateUser(pass, profile, callback);
+        }
+      }
+    });
+  };
+
+})();
+
+module.exports = user;
