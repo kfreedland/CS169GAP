a2a8de3ee2807afd506b0f26fd9ffe4b1ebb1f15
Evan Layman <evan_layman@berkeley.edu>
2013-03-08 10:55:27 -0800

    trying to change back to how the code was but still returning responseDict

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -26,13 +26,44 @@ var Users = function () {
       , user = geddy.model.User.create(params)
       , sha;
 
-    var callback = function (errCode){
-        var responseDict = {};
-        responseDict['errCode'] = errCode;
-        self.respond(responseDict, {format: 'json'});
-    };
+      geddy.model.User.first({username: user.username}, function(err, data) {
+      if (data) {
+        params.errors = {
+          username: 'This username is already in use.'
+        };
+        //User exists errCode = 2
+        callback(2);
+        // self.transfer('add');
+      }
+      else {
+        if (user.isValid()) {
+          user.password = cryptPass(user.password);
+        }
+        user.save(function(err, data) {
+          if (err) {
+            params.errors = err;
+            consol.log("got error: " + err);
+            callback(err);
+            // self.transfer('add');
+          }
+          else {
+            //Success errCode = 1
+            callback(1);
+            // self.redirect({controller: self.name});
+          }
+        });
+      }
+    });
 
-    geddy.model.User.create(user, callback);
+
+
+    // var callback = function (errCode){
+    //     var responseDict = {};
+    //     responseDict['errCode'] = errCode;
+    //     self.respond(responseDict, {format: 'json'});
+    // };
+
+    // geddy.model.User.create(user, callback);
 
   };
 
