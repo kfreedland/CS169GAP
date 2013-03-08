933053f00578f787e6ecc230e80367dbfc1523ad
Travis Langner <tlangner1@gmail.com>
2013-03-08 12:38:20 -0800

    working on create activity, almost done

--- app/models/activity.js
+++ app/models/activity.js
@@ -25,35 +25,98 @@ var Activity = function () {
 
     var self = this;
 
+    var validCategories = new Array("sports", "entertainment", "concert");
+
+
+    //Parse strings to Ints
+    if(parameterDict.time1){
+      parameterDict.time1 = parseInt(parameterDict.time1,10)
+    } 
+    if(parameterDict.time2){
+      parameterDict.time2 = parseInt(parameterDict.time2,10)
+    } 
+    if(parameterDict.begin_date){
+      parameterDict.begin_date = parseInt(parameterDict.begin_date,10)
+    } 
+    if(parameterDict.end_date){
+      parameterDict.end_date = parseInt(parameterDict.end_date,10)
+    } 
+    if(parameterDict.low_price){
+      parameterDict.low_price = parseInt(parameterDict.low_price,10)
+    }
+    if(parameterDict.high_price){
+      parameterDict.high_price = parseInt(parameterDict.high_price,10)
+    }
+    if(parameterDict.low_num_participants){
+      parameterDict.low_num_participants = parseInt(parameterDict.low_num_participants,10)
+    }
+    if(parameterDict.high_num_participants){
+      parameterDict.high_num_participants = parseInt(parameterDict.high_num_participants,10)
+    }
+    if(parameterDict.latitude){
+      parameterDict.latitude = parseFloat(parameterDict.latitude)
+    }
+    if(parameterDict.longitude){
+      parameterDict.longitude = parseFloat(parameterDict.longitude)
+    }
+
     //make sure required fields are non-null
     if (parameterDict.name == null){
-      callback({"errCode": 6});    
+      callback({"errCode": 6, "message": "null name"}); 
+
     } else if (parameterDict.flag == null){
-      callback({"errCode": 6});    
+      callback({"errCode": 6, "message": "null flag"});  
+
     } else if (parameterDict.flag == 'start_end' || paramaterDict.flag == 'open_close'){
-      if(parameterDict.time1 == null || parameterDict.time2 == null){
-        callback({"errCode": 6});    
+      if(parameterDict.time1 == null){
+        callback({"errCode": 6, "message": "null time2"});    
       }
-    } else if(parameterDict.flag == null){
-      callback({"errCode": 6}); 
+      if(parameterDict.time2 == null){
+        callback({"errCode": 6, "message": "null time2"});    
+      }
+
     } else if (parameterDict.flag != 'start_end' && parameterDict.flag != 'open_close' 
            && parameterDict.flag != 'any_time' &&  parameterDict.flag != 'day_time' && parameterDict.flag != 'night_time'){
-      callBack{"errCode":6}
-    }
+      callback({"errCode": 6, "message": "invalid flag"});    
 
-    
+    } else if (parameterDict.low_price == null){
+      callback({"errCode": 6, "message": "null low_price"});   
 
+    } else if (parameterDict.high_price == null){
+      callback({"errCode": 6, "message": "null high_price"});   
+
+    } else if(parameterDict.low_price != null && parameterDict.high_price != null){
+
+      if (parameterDict.low_price > parameterDict.high_price){
+        callback({"errCode": 6, "message": "invalid prices"});  
+      }
+
+    } else if(parameterDict.low_num_participants != null && parameterDict.high_num_participants != null){
+
+      if (parameterDict.low_num_participants > parameterDict.high_num_participants){
+        callback({"errCode": 6, "message": "invalid participants"});  
+      } 
+
+    } else if(parameterDict.category == null){
+      callback({"errCode": 6, "message": "null category"});  
+
+    } else if(acceptedCategories.indexOf(parameterDict.category) == -1){
+      callback({"errCode": 6, "message": "invalid category"});  
+
+    } else {
+
+      //all checks pass
       var newActivity = geddy.model.Activity.create(parameterDict);
-            geddy.model.Activity.save(newActivity, 
-              function (err, result){
+      geddy.model.Activity.save(newActivity, 
+        function (err, result){
 
-                if(err){
-                  callback({"errCode":-1});
-                } else {
-                  callback ({"errCode": 1});
-                }
-              });
-    }
+          if(err){
+            callback({"errCode":7});
+          } else {
+            callback ({"errCode": 1});
+          }
+        });
+    }      
   };
 
 var geoSearchHelper = function(records, lat, long, callback)
