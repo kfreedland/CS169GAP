de08ae21f2a3b7930f531b73d3a845df28379050
Evan Layman <evan_layman@berkeley.edu>
2013-03-14 21:28:38 -0700

    merged with master

--- public/js/core/models.js
+++ public/js/core/models.js
@@ -48,18 +48,15 @@ var geoSearchHelper = function (records, lat, long, callback)
       break;
     }
   }
-<<<<<<< HEAD
-  returnRecords.sort(function (recA, recB) {return recA.dist - recB.dist});
-=======
   returnRecords.sort(function (recA, recB) {return recA.dist - recB.dist;});
->>>>>>> f68a669f3af287984df40cc1ad31ace2e17ac1e9
   callback(returnRecords, count);
 };
 
 Activity.add = function (parameterDict, callback){
 
-    var self = this
-      , respDict = {format: 'json'};
+  var self = this;
+
+    var respDict = {};
 
     console.log("reached model create");
     console.dir(parameterDict);
@@ -98,8 +95,8 @@ Activity.add = function (parameterDict, callback){
     if (parameterDict.longitude !== undefined) {
       parameterDict.longitude = parseFloat(parameterDict.longitude);
     }
-    if (parameterDict.duration !== undefined) {
-      parameterDict.longitude = parseFloat(parameterDict.duration);
+    if(parameterDict.duration != undefined){
+      parameterDict.duration = parseFloat(parameterDict.duration);
     }
 
     //make sure required fields are defineed
@@ -125,11 +122,7 @@ Activity.add = function (parameterDict, callback){
         callback(respDict);
         return;
       }
-<<<<<<< HEAD
-      if(parameterDict.time2 == undefined) {
-=======
       if(parameterDict.time2 === undefined) {
->>>>>>> f68a669f3af287984df40cc1ad31ace2e17ac1e9
 
         respDict.errCode = 6;
         respDict.message = "null time2";
@@ -228,21 +221,12 @@ Activity.add = function (parameterDict, callback){
       activityDict.end_date = parameterDict.end_date;
     }
     console.log("parameterDict.low_price = " + parameterDict.low_price);
-<<<<<<< HEAD
-    if(parameterDict.low_price != undefined){
-      console.log("low_price is NOT undefined!!");
-      activityDict.low_price = parameterDict.low_price;
-    } 
-    console.log("parameterDict.high_price = " + parameterDict.high_price); 
-    if(parameterDict.high_price != undefined){
-=======
     if(parameterDict.low_price !== undefined){
       console.log("low_price is NOT undefined!!");
       activityDict.low_price = parameterDict.low_price;
     }
     console.log("parameterDict.high_price = " + parameterDict.high_price); 
     if(parameterDict.high_price !== undefined){
->>>>>>> f68a669f3af287984df40cc1ad31ace2e17ac1e9
       console.log("high_price is NOT undefined!!");
       activityDict.high_price = parameterDict.high_price;
     }  
@@ -263,28 +247,6 @@ Activity.add = function (parameterDict, callback){
     }
 
 
-<<<<<<< HEAD
-    //all checks pass
-    console.dir(activityDict);
-
-    var activityRecord = geddy.model.Activity.create(activityDict);
-
-    console.dir(activityRecord);
-
-    geddy.model.Activity.save(activityRecord, 
-      function (err, result){
-
-        if(err){
-
-          respDict.errCode = 7;
-          respDict.message = "database error";
-          callback(respDict);
-        } else {
-
-
-          respDict.errCode = 1;
-          callback(respDict);
-=======
     //Make sure does not exist
     geddy.model.Activity.load(activityDict, 
       function (err, result) {
@@ -321,7 +283,6 @@ Activity.add = function (parameterDict, callback){
                 callback(respDict);
               }
             });
->>>>>>> f68a669f3af287984df40cc1ad31ace2e17ac1e9
         }
 
     });
