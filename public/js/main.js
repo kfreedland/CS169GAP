0931ff0374f25e68246bb2d17e330f5119dd6049
Evan Layman <evan_layman@berkeley.edu>
2013-03-21 13:49:05 -0700

    turned off jquery mobile handling of facebook button so it works as expected

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,3 @@
-//Login Page
-function facebookClicked(event) {
-  	event.preventDefault();
-	//var url = document.location.protocol + window.location.host + $('.btn-facebook').attr('href');
-	var url = $('.btn-facebook').attr('href');
-	$(location).attr('href', url);
-	// $(href + $('.btn-facebook').attr('href') ).show();
-    return false;
-}
-
 //Remove any parameters from the URL when changing views
 $('#findActivityButton').live("click tap", function() {
 	//If you are on another URL, this button should go back to main url
