0aebb6b01014fe711d7f8cac83fadf5eb4e8a14b
Kurtis <kfreedland@berkeley.edu>
2013-04-01 19:25:19 -0700

    more tests

--- test/eventAddTest.js
+++ test/eventAddTest.js
@@ -134,4 +134,187 @@ describe('Event', function()
 			});
 		});
 	});
+
+	describe('Event.add registered', function()
+	{
+		it('should return errCode:1', function(done)
+		{
+		    var eventDict = {};
+		    eventDict.name = 'jogging';
+		    eventDict.description = 'go for a run with some friends!';
+		    eventDict.category = 'Sports';
+		    eventDict.time1 = undefined;
+		    eventDict.time2 = undefined;
+		    eventDict.flag = 'anyTime';
+		    eventDict.begindate = undefined;
+		    eventDict.enddate = undefined;
+		    eventDict.lowprice = '0';
+		    eventDict.highprice = '0';
+		    eventDict.lownumparticipants = '1';
+		    eventDict.highnumparticipants = undefined;
+		    eventDict.latitude = undefined;
+		    eventDict.longitude = undefined;
+		    eventDict.duration = '2';
+
+		    Activity.add(eventDict, function(err, response)
+		    {
+		    	var user = User.create({username: 'theGracken',
+		                    password: 'MyPassword!',
+		                    confirmPassword: 'MyPassword!',
+		                    familyName: 'LastName1',
+		                    givenName: 'FirstName1',
+		                    email: 'kfreedland@berkeley.edu'});
+				User.add(user, function (answerDict) 
+				{
+					var eventData = {};
+					var expected = {errCode: 1};
+					User.first({username: 'foo'}, function(err, userRecord)
+					{
+						var uId = userRecord.id;
+
+						Activity.first({name: 'jogging'}, function(err, activityRecord)
+						{
+							var d = new Date();
+							eventData.name ="magical orgy";
+							eventData.activityid = activityRecord.id;
+							eventData.time1 = 500;
+							eventData.time2 = 1000;
+							eventData.startdate = d.getTime();
+							eventData.enddate = d.getTime() + 50000;
+							eventData.description = 'my Event';
+							eventData.attendingusers = userRecord.username;
+
+							Event.add(eventData, function(respDict)
+							{
+								assert.deepEqual(respDict, expected);
+								done();
+							});
+						});
+					});
+				});
+			});
+		});
+	});
+
+	describe('Event.add invalid start/enddate', function()
+	{
+		it('should return errCode:8', function(done)
+		{
+		    var eventDict = {};
+		    eventDict.name = 'jogging';
+		    eventDict.description = 'go for a run with some friends!';
+		    eventDict.category = 'Sports';
+		    eventDict.time1 = undefined;
+		    eventDict.time2 = undefined;
+		    eventDict.flag = 'anyTime';
+		    eventDict.begindate = undefined;
+		    eventDict.enddate = undefined;
+		    eventDict.lowprice = '0';
+		    eventDict.highprice = '0';
+		    eventDict.lownumparticipants = '1';
+		    eventDict.highnumparticipants = undefined;
+		    eventDict.latitude = undefined;
+		    eventDict.longitude = undefined;
+		    eventDict.duration = '2';
+
+		    Activity.add(eventDict, function(err, response)
+		    {
+		    	var user = User.create({username: 'foo',
+		                    password: 'MyPassword!',
+		                    confirmPassword: 'MyPassword!',
+		                    familyName: 'LastName1',
+		                    givenName: 'FirstName1',
+		                    email: 'kfreedland@berkeley.edu'});
+				User.add(user, function (answerDict) 
+				{
+					var eventData = {};
+					var expected = {errCode: 8};
+					User.first({username: 'foo'}, function(err, userRecord)
+					{
+						var uId = userRecord.id;
+
+						Activity.first({name: 'jogging'}, function(err, activityRecord)
+						{
+							var d = new Date();
+							eventData.name ="magical orgy";
+							eventData.activityid = activityRecord.id;
+							eventData.time1 = 1000;
+							eventData.time2 = 500;
+							eventData.startdate = d.getTime() + 50000;
+							eventData.enddate = d.getTime();
+							eventData.description = 'my Event';
+							eventData.attendingusers = userRecord.email;
+
+							Event.add(eventData, function(respDict)
+							{
+								assert.deepEqual(respDict, expected);
+								done();
+							});
+						});
+					});
+				});
+			});
+		});
+	});
+
+	describe('Event.add not a recordid', function()
+	{
+		it('should return errCode:9', function(done)
+		{
+		    var eventDict = {};
+		    eventDict.name = 'jogging';
+		    eventDict.description = 'go for a run with some friends!';
+		    eventDict.category = 'Sports';
+		    eventDict.time1 = undefined;
+		    eventDict.time2 = undefined;
+		    eventDict.flag = 'anyTime';
+		    eventDict.begindate = undefined;
+		    eventDict.enddate = undefined;
+		    eventDict.lowprice = '0';
+		    eventDict.highprice = '0';
+		    eventDict.lownumparticipants = '1';
+		    eventDict.highnumparticipants = undefined;
+		    eventDict.latitude = undefined;
+		    eventDict.longitude = undefined;
+		    eventDict.duration = '2';
+
+		    Activity.add(eventDict, function(err, response)
+		    {
+		    	var user = User.create({username: 'foo',
+		                    password: 'MyPassword!',
+		                    confirmPassword: 'MyPassword!',
+		                    familyName: 'LastName1',
+		                    givenName: 'FirstName1',
+		                    email: 'kfreedland@berkeley.edu'});
+				User.add(user, function (answerDict) 
+				{
+					var eventData = {};
+					var expected = {errCode: 9};
+					User.first({username: 'foo'}, function(err, userRecord)
+					{
+						var uId = userRecord.id;
+
+						Activity.first({name: 'jogging'}, function(err, activityRecord)
+						{
+							var d = new Date();
+							eventData.name ="magical orgy";
+							eventData.activityid = 'prettysurethisisntvalid';
+							eventData.time1 = 500;
+							eventData.time2 = 1000;
+							eventData.startdate = d.getTime();
+							eventData.enddate = d.getTime() + 50000;
+							eventData.description = 'my Event';
+							eventData.attendingusers = userRecord.email;
+
+							Event.add(eventData, function(respDict)
+							{
+								assert.deepEqual(respDict, expected);
+								done();
+							});
+						});
+					});
+				});
+			});
+		});
+	});
 });
