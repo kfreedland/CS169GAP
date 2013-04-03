74ad078f4199cf5f800c14a32da2fe01421c3d93
Travis Langner <tlangner1@gmail.com>
2013-04-02 17:40:21 -0700

    wrote more tests for add activity

--- test/eventChangeDateTimeTest.js
+++ test/eventChangeDateTimeTest.js
@@ -9,8 +9,37 @@ var resetFixture = function (done){
 	});
 };
 
-describe('eventChangeDateTime', function(){
-	before(function(done) {
-		//Erase database
-		resetFixture(done);
-	});
+// describe('eventChangeDateTime', function(){
+// 	before(function(done) {
+// 		//Erase database
+// 		resetFixture(done);
+// 	});
+
+
+// 	describe('event.changeDateTime normal', function(){
+// 		it('should return errCode:1', function(done){
+// 		    var activityDict = {};
+// 		    activityDict.name = 'jogging';
+// 		    activityDict.description = 'go for a run with some friends!';
+// 		    activityDict.category = 'Sports';
+// 		    activityDict.time1 = undefined;
+// 		    activityDict.time2 = undefined;
+// 		    activityDict.flag = 'anyTime';
+// 		    activityDict.begindate = undefined;
+// 		    activityDict.enddate = undefined;
+// 		    activityDict.lowprice = '0';
+// 		    activityDict.highprice = '0';
+// 		    activityDict.lownumparticipants = '1';
+// 		    activityDict.highnumparticipants = undefined;
+// 		    activityDict.latitude = undefined;
+// 		    activityDict.longitude = undefined;
+// 		    activityDict.duration = '2';
+
+// 		    Activity.add(activityDict, function(response)
+// 		    {
+// 	    		var expected = {errCode: 1};
+// 	    		assert.deepEqual(response,expected);
+// 	    		done();
+// 		    });
+// 		});
+// 	});
