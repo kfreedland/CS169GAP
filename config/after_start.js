59e7826e90977cf5d22bfb0e5f941a074e1f1263
Evan Layman <evan_layman@berkeley.edu>
2013-04-13 18:57:16 -0700

    Fixed typo on geddy.io when setting up config for heroku

--- config/after_start.js
+++ config/after_start.js
@@ -2,12 +2,12 @@
 //Setup io
 
  // var io = require('socket.io').listen(8000);
-var port = process.env.PORT || 8000;
+var port = 8000//process.env.PORT || 8000;
 geddy.io = require('socket.io').listen(port);
 //Configue io to work with heroku
 geddy.io.configure(function () { 
-	io.set("transports", ["xhr-polling"]); 
-	io.set("polling duration", 10); 
+	geddy.io.set("transports", ["xhr-polling"]); 
+	geddy.io.set("polling duration", 10); 
 });
 // geddy.io.sockets.on('connection', function(socket) {
 // 	geddy.socket = socket;
