78cdb03cc732c01027925450c022ba15da84a395
Evan Layman <evan_layman@berkeley.edu>
2013-04-13 18:53:41 -0700

    Changed socket io listener to use process.env.PORT

--- config/after_start.js
+++ config/after_start.js
@@ -2,7 +2,8 @@
 //Setup io
 
  // var io = require('socket.io').listen(8000);
-geddy.io = require('socket.io').listen(8000);
+var port = process.env.PORT || 8000;
+geddy.io = require('socket.io').listen(port);
 //Configue io to work with heroku
 geddy.io.configure(function () { 
 	io.set("transports", ["xhr-polling"]); 
