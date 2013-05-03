b71558eec477f71356305b6a37ddbf04a0933a35
Josh Ahn <joshahn@Joshua-Yims-MacBook-Pro.local>
2013-05-03 16:45:52 -0700

    added selenium tests for iteration 4, does real time comments viewing

--- front-end-tests/selenium/real_time_comments.py
+++ front-end-tests/selenium/real_time_comments.py
@@ -5,6 +5,19 @@ from selenium.webdriver.common.keys import Keys
 import time
 from initialize import SeleniumTest 
 
-def real_time_comments(driver):
-	my_account = driver.find_element_by_xpath("span[class='full_name']")
+def go_to_events(driver):
+	my_account = driver.browser.find_element_by_xpath("//span[contains(@class,'full_name')]")
 	my_account.click()
+	time.sleep(1)
+
+	first_event = driver.browser.find_element_by_id("event_current1")
+	first_event.click()
+
+def write_comment(driver):
+	comment_box = driver.browser.find_element_by_id("commentTextarea")
+	comment_box.send_keys("I love Selenium Testing, can you see this in real time?")
+
+def submit_comment(driver):
+	comment_button = driver.browser.find_element_by_id("addCommentButton")
+	comment_button.click()
+
